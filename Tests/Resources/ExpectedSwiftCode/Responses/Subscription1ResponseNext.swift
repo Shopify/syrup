658675ed5fd4ee292b8ab2b0a7121171c6251f31
fredells <fred.ells@shopify.com>
2020-10-20 11:21:26 -0400

    Fix typename on subscription response root that causes decoding to fail

--- Tests/Resources/ExpectedSwiftCode/Responses/Subscription1ResponseNext.swift
+++ Tests/Resources/ExpectedSwiftCode/Responses/Subscription1ResponseNext.swift
@@ -6,9 +6,6 @@ struct Subscription1Response: GraphApiResponse, Equatable {
 	// MARK: - Response Fields
 		public var presenceChanged: Bool
 
-	// MARK: - Helpers
-	public let __typename: String
-
 	public static let customDecoder: JSONDecoder = MerchantApi.customDecoder
 	public static let customEncoder: JSONEncoder = MerchantApi.customEncoder
 
